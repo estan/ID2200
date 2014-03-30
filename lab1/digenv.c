957ba14a25dfc29b00c310b5eb02115311694b60
Elvis Stansvik <elvstone@gmail.com>
2014-03-30 17:05:52 +0200

    Improve comments.

--- lab1/digenv.c
+++ lab1/digenv.c
@@ -6,9 +6,7 @@
 int main(int argc, char *argv[]) {
     char *pager_env = getenv("PAGER");
 
-    /* Construct pipeline. */
-    
-    /*                    file        argv                        err  next    fallback */
+    /* Create pipeline    file        argv                        err  next    fallback */
     command_t more     = {"more",     (char *[]){"more", NULL},     0, NULL,   NULL};
     command_t less     = {"less",     (char *[]){"less", NULL},     0, NULL,   &more};
     command_t pager    = {pager_env,  (char *[]){pager_env, NULL},  0, NULL,   &less};
@@ -16,13 +14,14 @@ int main(int argc, char *argv[]) {
     command_t grep     = {"grep",     argv,                         0, &sort,  NULL};
     command_t printenv = {"printenv", (char *[]){"printenv", NULL}, 0, &sort,  NULL};
 
+    argv[0] = "grep"; /* Since we reuse argv for grep */
+
     if (argc > 1) {
-        /* Arguments given: Run grep as well. */
+        /* Arguments given: Run grep as well */
         printenv.next = &grep;
-        argv[0] = "grep";
     }
 
-    /* Run pipeline. */
+    /* Run pipeline */
     run_pipeline(&printenv, STDIN_FILENO);
 
     return 0;
