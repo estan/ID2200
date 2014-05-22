22fd3d429ee81a0d773d30b9a7b80741bdac8aba
Elvis Stansvik <elvstone@gmail.com>
2014-05-22 20:38:45 +0200

    Use CLOCK_MONOTONIC instead of CLOCK_MONOTONIC_RAW.
    shell.it.kth.se doesn't have _RAW.

--- lab3/timer.h
+++ lab3/timer.h
@@ -4,8 +4,8 @@
 struct timespec t0, t1; /* Start/end time for timer */
 
 /* Timer macros */
-#define TIMER_START() clock_gettime(CLOCK_MONOTONIC_RAW, &t0)
-#define TIMER_STOP() clock_gettime(CLOCK_MONOTONIC_RAW, &t1)
+#define TIMER_START() clock_gettime(CLOCK_MONOTONIC, &t0)
+#define TIMER_STOP() clock_gettime(CLOCK_MONOTONIC, &t1)
 #define TIMER_ELAPSED_NS() \
     (t1.tv_sec * 1000000000 + t1.tv_nsec) - \
     (t0.tv_sec * 1000000000 + t0.tv_nsec)
